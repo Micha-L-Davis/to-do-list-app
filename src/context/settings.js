e34660d42c046f24c7cd2605b3df4b91f593f78f
Micha Davis <m.leigh.davis@gmail.com>
2022-05-03 17:48:06 -0700

    refactor settings to use reducer

--- src/context/settings.js
+++ src/context/settings.js
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 export const SettingsContext = React.createContext();
 
+export const initialState = {
+  hideCompleted: true,
+  numToDisplay: 3,
+  defaultSortField: 'DIFFICULTY'
+}
+
+export const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'TOGGLE_HIDE':
+      return { ...state, hideCompleted: !state.hideCompleted };
+    case 'DISPLAY_NUM':
+      if (+payload > 1)
+        return { ...state, numToDisplay: payload }
+      else
+        return state;
+    case 'SORT':
+      return { ...state, defaultSortField: payload }
+    default:
+      return state;
+  }
+}
+
 function SettingsProvider({ children }) {
-  const [hideCompleted, setHideCompleted] = useState(true);
-  const [numToDisplay, setNumToDisplay] = useState(3);
-  const [defaultSortField, setSortField] = useState('NAME');
+  let [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <SettingsContext.Provider value={{ hideCompleted, numToDisplay, defaultSortField }}>
+    <SettingsContext.Provider value={{ state, dispatch }}>
       {children}
     </SettingsContext.Provider>
   )
