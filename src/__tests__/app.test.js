44087eedd2692c55232f2e9d12f444756bf0bfd9
Micha Davis <m.leigh.davis@gmail.com>
2022-05-03 21:22:25 -0700

    write tests

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,25 +1,56 @@
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Main from '../index'
+import App from '../app'
+import SettingsProvider from '../context/settings'
+import SettingsContext from '../context/settings'
 
 describe('To-Do App Tests', () => {
-  render(<Main />)
+  render(
+    <SettingsProvider>
+      <App />
+      <SettingsContext.Consumer>
+        {({ settings, settingsDispatch }) => {
+          <>
+            <button
+              data-testid='toggle-button'
+              onClick={(() => settingsDispatch({
+                type: 'TOGGLE_HIDE',
+                payload: !settings.hideCompleted
+              }))}
+            ></button>
+            <button
+              data-testid='number-set'
+              onClick={(() => settingsDispatch({
+                type: 'DISPLAY_NUM',
+                payload: 4,
+              }))}
+            ></button>
+          </>
+        }}
+      </SettingsContext.Consumer>
+    </SettingsProvider>
+  )
   let form = screen.getByTestId('form');
 
 
-  it('should render a maximum of three items', () => {
-    fireEvent.submit(form, { target: { text: 'test description', assignee: 'test person', difficulty: 5 } })
-    fireEvent.submit(form, { target: { text: 'test description', assignee: 'test person', difficulty: 4 } })
-    fireEvent.submit(form, { target: { text: 'test description', assignee: 'test person', difficulty: 3 } })
-    fireEvent.submit(form, { target: { text: 'test description', assignee: 'test person', difficulty: 2 } })
+  it('should render three items and hide the fourth', () => {
+    fireEvent.submit(form, { target: { text: 'test 1', assignee: 'test person', difficulty: 1 } })
+    fireEvent.submit(form, { target: { text: 'test 2', assignee: 'test person', difficulty: 2 } })
+    fireEvent.submit(form, { target: { text: 'test 3', assignee: 'test person', difficulty: 3 } })
+    fireEvent.submit(form, { target: { text: 'test 4', assignee: 'test person', difficulty: 4 } })
 
     let firstElement = screen.getByTestId('element-0');
     let lastElement = screen.getByTestId('element-2');
-    let nonexistentElement = screen.getByTestId('element-3');
 
     expect(firstElement).toBeTruthy();
     expect(lastElement).toBeTruthy();
-    expect(nonexistentElement).toBeFalsy();
+  });
+  it('should reveal the fourth when the display num is increased', () => {
+    const button = screen.getByTestId('number-set');
+    fireEvent.click(button);
+    let hiddenElement = screen.getByTestId('element-3')
+
+    expect(hiddenElement).toBeTruthy();
   });
 });
 
