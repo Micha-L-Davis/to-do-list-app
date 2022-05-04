6bb95fdb46c65f9c06d9c7364faa4205008b7ca5
Micha Davis <m.leigh.davis@gmail.com>
2022-05-03 21:23:26 -0700

    fix duplicate key error

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,10 +1,10 @@
+import './list.scss';
+
 const List = (props) => {
-
-
   return (
     props.list.map((item, index) => {
       return (
-        <div key={item.id} data-testId={`element-${index}`}>
+        <div key={`${item.id}-${index}`} data-testid={`element-${index}`}>
           <p>{item.text}</p>
           <p><small>Assigned to: {item.assignee}</small></p>
           <p><small>Difficulty: {item.difficulty}</small></p>
