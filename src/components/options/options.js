6c7c17e7e1db5d6c283ae3e1bb14c7bc3b654d55
Micha Davis <m.leigh.davis@gmail.com>
2022-05-03 21:24:09 -0700

    create options form component

--- src/components/options/options.js
+++ src/components/options/options.js
@@ -1,16 +1,32 @@
 import { useContext } from 'react';
-import { FormGroup, InputGroup, Switch } from "@blueprintjs/core";
+import { FormGroup, NumericInput, Switch } from "@blueprintjs/core";
 import { SettingsContext } from "../../context/settings";
+import './options.scss'
 
 
 function Options(props) {
-  const settings = useContext(SettingsContext);
+  const { settings, settingsDispatch } = useContext(SettingsContext);
+
+  function handleSwitchChange(event) {
+    event.preventDefault();
+    //console.log(event.target.checked);
+    settingsDispatch({ type: 'TOGGLE_HIDE', payload: null });
+  }
+
+  function handleNumberChange(number) {
+    settingsDispatch({ type: 'DISPLAY_NUM', payload: number })
+  }
 
   return (
     <>
       <FormGroup label='Display Options'>
-        <Switch checked={settings.hideCompleted} label='Hide Completed Items' /*onChange={}*/ />
-        <InputGroup type='text' placeholder={settings.numToDisplay} label='Max # items to display' /*onChange={}*/ />
+        <Switch
+          checked={settings.hideCompleted}
+          inline='true' alignIndicator='Alignment.LEFT'
+          label='Hide Completed Items'
+          onChange={handleSwitchChange}
+        />
+        <NumericInput min={2} placeholder={settings.numToDisplay} label='Max # items to display' onValueChange={handleNumberChange} />
       </FormGroup>
     </>
   )
